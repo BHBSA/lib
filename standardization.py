d1ddc0cf616f35cf8f0c1b14cbc53645a5ef56b1
jijunyu <jijunyu@fangjia.com>
2018-05-25 14:02:51 +0800

    standard

--- standardization.py
+++ standardization.py
@@ -7,9 +7,7 @@ from lib.region_block import region_block
 
 with open('lib/city.json', 'r+') as f:
     standard_city_dict = json.loads(f.read())
-# with open('region.json', 'r+') as f:
-#     standard_block_dict = json.loads(f.read())
-#     print(standard_block_dict)
+
 standard_block_dict  = region_block()
 def standard_city(city_name):
     """
@@ -20,7 +18,6 @@ def standard_city(city_name):
     for i in standard_city_dict.items():
         for city in i[1]:
             if city and i[0] in city_name:
-                # print(i[0])
                 return i[0]
             else:
                 continue
@@ -40,13 +37,11 @@ def standard_block(city_name,region_name):
             for block in i[1].items():
                 for n in block[1]:
                     if n in region_name:
-                        # print(block[0])
                         return block[0]
 
                 else:
                     continue
     print('无法标准化')
-    # print(region_name)
     return False
 
 
