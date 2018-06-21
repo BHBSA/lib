9e5f5944b41ba69e8c0065e8e05410d2f032f0ad
jijunyu <jijunyu@fangjia.com>
2018-06-21 15:53:26 +0800

    standardization

--- standardization.py
+++ standardization.py
@@ -3,11 +3,16 @@
 """
 
 import json
-from lib.region_block import region_block
-from lib.log import LogHandler
+# from lib.region_block import region_block
+# from lib.log import LogHandler
+
+from region_block import region_block
+from log import LogHandler
 
 log = LogHandler(__name__)
-with open('lib/city.json', 'r+') as f:
+# with open('lib/city.json', 'r+') as f:
+#     standard_city_dict = json.loads(f.read())
+with open('city.json', 'r+') as f:
     standard_city_dict = json.loads(f.read())
 
 standard_block_dict = region_block()
@@ -20,11 +25,13 @@ def standard_city(city_name):
     :return: 城市名称
     """
     for i in standard_city_dict.items():
-        for city in i[1]:
-            if city and i[0] in city_name:
+        print(i)
+        for k in i[1]:
+            if k in city_name or i[0] in city_name:
                 return True, i[0]
-            else:
-                continue
+        # for city in i[1]:
+        #     if city and i[0] in city_name:
+        #         return True, i[0]
     log.error("城市无法标准化：{}".format(city_name))
     return False, city_name
 
@@ -48,5 +55,6 @@ def standard_block(city_name, region_name):
 
 
 if __name__ == '__main__':
-    # standard_city('   珠海市  ')
-    standard_block('北京', '[朝阳]百子湾')
+    a, b = standard_city('  玉都  ')
+    print(b)
+    # standard_block('北京', '[朝阳]百子湾')
